c20ce8156658d683fd04492e470d8a8507bf7ca2
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-01 08:51:29 -0500

    Create instpostgre.sh
    Postgres Installer

---
+++ extras/instpostgre.sh
@@ -0,0 +1,10 @@
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+sudo apt update
+sudo apt install postgresql-16
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/16/main/postgresql.conf
+sudo sed -i '/^host/s/ident/md5/' /etc/postgresql/16/main/pg_hba.conf
+sudo sed -i '/^local/s/peer/trust/' /etc/postgresql/16/main/pg_hba.conf
+echo "host all all 0.0.0.0/0 md5" | sudo tee -a /etc/postgresql/16/main/pg_hba.conf
+sudo systemctl start postgresql
+sudo systemctl enable postgresql
