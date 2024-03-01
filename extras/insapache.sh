9a2fb3a816db7c10170e6912f13a85f1cc17196f
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-01 08:44:41 -0500

    Create insapache.sh
    Apache installer

---
+++ extras/insapache.sh
@@ -0,0 +1,11 @@
+sudo add-apt-repository ppa:ondrej/apache2
+sudo apt-get update
+sudo apt-get install apache2
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
+curl https://oss-binaries.phusionpassenger.com/auto-software-signing-gpg-key.txt | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/phusion.gpg >/dev/null
+sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger jammy main > /etc/apt/sources.list.d/passenger.list'
+sudo apt-get update
+sudo apt install -y libapache2-mod-passenger libapache2-mod-xsendfile
+sudo a2enmod proxy_http rewrite passenger ssl
+sudo apache2ctl restart
+sudo systemctl restart apache2
