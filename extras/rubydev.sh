1b82bf16de8a03a4ff110eee7dae7f4f37255842
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-01 09:33:51 -0500

    Create rubydev.sh
    Rubydev INstaller

---
+++ extras/rubydev.sh
@@ -0,0 +1,3 @@
+sudo add-apt-repository -y ppa:instructure/ruby
+sudo apt-get install zlib1g-dev libxml2-dev libsqlite3-dev libpq-dev libxmlsec1-dev libyaml libyaml-dev libidn11-dev curl make g++
+sudo apt-get update
