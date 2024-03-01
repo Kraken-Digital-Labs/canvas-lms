3c149ed0956d2122f80190fc6d74b106dfb7188d
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-01 07:25:30 -0500

    Create eco.sh
    Generator of Secret and Keys

---
+++ extras/eco.sh
@@ -0,0 +1,6 @@
+cd /var/canvas
+cp .env.example .env
+ECOSYSTEM_SECRET=$(openssl rand -hex 32)
+ECOSYSTEM_KEY=$(openssl rand -hex 32)
+CIPHER_PASSWORD=$(openssl rand -hex 16)
+sed -i "s/^\(NODE_ENV=\).*/\1production/; s/^\(ECOSYSTEM_SECRET=\).*/\1$ECOSYSTEM_SECRET/; s/^\(ECOSYSTEM_KEY=\).*/\1$ECOSYSTEM_KEY/; s/^\(CIPHER_PASSWORD=\).*/\1$CIPHER_PASSWORD/" .env
