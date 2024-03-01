ee8dfc34ca4bbacf6988bab08f3cfe6128c10fbc
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-01 08:35:32 -0500

    Create v.sh
    versions

---
+++ extras/v.sh
@@ -0,0 +1,15 @@
+git --version
+ruby -v
+rvm -v
+bundler -v
+yarn -v
+passenger -v
+node -v
+npm –version
+nvm -v
+nvm ls
+nvm ls-remote
+redis-server -v
+sudo systemctl status redis
+sudo apachectl -V          
+sudo apachectl status
