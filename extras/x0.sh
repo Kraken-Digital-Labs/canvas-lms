b1681f98c7c3696c683870a7429b835b15f535af
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-01 08:57:29 -0500

    Create x0.sh
    Installers

---
+++ extras/x0.sh
@@ -0,0 +1,1 @@
+sudo apt install build-essential software-properties-common git git-core curl wget make g++ dirmngr gnupg apt-transport-https ca-certificates certbot python3-certbot-apache screen gnupg2 openssl dnsmasq xmlsec1 libyaml
