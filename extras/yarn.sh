68340601c4c916822148ef48cee8b15a9fe305ff
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-01 09:08:34 -0500

    Create yarn.sh
    Yarn Installer

---
+++ extras/yarn.sh
@@ -0,0 +1,5 @@
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo gpg --dearmor -o /usr/share/keyrings/yarnkey.gpg
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list > /dev/null
+sudo apt update
+sudo apt install -y yarn=1.19.1-1
+sudo npm -g install yarn
