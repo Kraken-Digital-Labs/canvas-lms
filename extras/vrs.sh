34d4cc2c4bf1891ba3e12a5f45453c6b8315301e
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-02 17:51:13 -0500

    Create vrs.sh
    update

---
+++ extras/vrs.sh
@@ -0,0 +1,15 @@
+git --version
+ruby -v
+rvm -v
+bundler -v
+yarn -v
+passenger -v
+node -v
+npm –version
+nvm -v
+nvm ls
+nvm ls-remote
+redis-server -v
+apache2 -v
+sudo systemctl status redis
+sudo systemctl status apache2
