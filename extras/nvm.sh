3a824787222a2ec4767e4382808ae961d31b134a
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-02 16:39:52 -0500

    Update nvm.sh
    update

--- extras/nvm.sh
+++ extras/nvm.sh
@@ -1,3 +1,8 @@
+export NVM_DIR="$HOME/.nvm" && (
+  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 source ~/.bashrc
 export NVM_DIR="$HOME/.nvm"
