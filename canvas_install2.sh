c9045f45500617087db2d8f65654810055c0af5a
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-02 17:52:46 -0500

    Update canvas_install2.sh
    update

--- canvas_install2.sh
+++ canvas_install2.sh
@@ -1,1 +1,3 @@
+bash
 curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/aio.sh | bash
+curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/vrs.sh | bash
