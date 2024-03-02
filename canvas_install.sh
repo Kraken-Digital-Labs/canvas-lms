6b27dcc741af15f3cf22eaad27d82c21af69b8a7
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-02 16:50:00 -0500

    Update canvas_install.sh
    update

--- canvas_install.sh
+++ canvas_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+bash
 wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/hst.sh
 sudo sh hst.sh
 curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/basic.sh | bash
@@ -7,3 +7,4 @@ curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master
 curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/srvr.sh | bash
 https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/canvas_install2.sh
 echo "Reboot the system & then run canvas_install2.sh"
+sudo reboot
