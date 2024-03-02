5e8d8a1dd3ad3c90f2939d0ed98c6419214b6d72
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-02 13:24:05 -0500

    Update aio.sh
    Update

--- extras/aio.sh
+++ extras/aio.sh
@@ -1,1 +1,14 @@
-nvm install v18.0.0 && nvm use v18.0.0 && nvm alias default v18.0.0 && npm install -g npm@latest && rvm install 3.1.0 && rvm use 3.1.0 --default && gem install rails --version 3.1.0 && sudo gem install bundler
+export vnd="18.0.0"
+export vrb="3.1.0"
+export vyr="1.19.1-1"
+nvm install v$vnd
+nvm use v$vnd
+nvm alias default v$vnd
+npm install -g npm@latest
+rvm install $vrb
+rvm use $vrb --default
+gem install rails --version $vrb
+sudo gem install bundler
+sudo apt install -y yarn=$vyr
+sudo npm -g install yarn
+#sudo reboot
