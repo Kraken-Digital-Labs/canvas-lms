c66acdf97b5e1f16434d3b97cad044b911d5e82c
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-02 17:19:11 -0500

    Update aio.sh
    update

--- extras/aio.sh
+++ extras/aio.sh
@@ -1,4 +1,8 @@
 curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/nvm.sh
+source ~/.bashrc
+source ~/.zshrc
+. ~/.profile
+source ~/.nvm
 nvm install v18.17.0
 nvm use v18.17.0
 nvm alias default v18.17.0
