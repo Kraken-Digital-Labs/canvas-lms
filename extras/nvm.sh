e82fc9f3f1a2e124b661e7ae733192a2cab5abd1
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-02 18:00:34 -0500

    Update nvm.sh
    Update

--- extras/nvm.sh
+++ extras/nvm.sh
@@ -8,6 +8,12 @@ source ~/.bashrc
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh  # This loads NVM
 source ~/.bashrc
+source ~/.bashrc
+source ~/.zshrc
+. ~/.profile
+source ~/.nvm
+source ~/.nvm/nvm.sh
 echo $NVM_DIR
 
