4daed1943d73e05a66b778682759e4ef7c24aabd
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-02 16:56:33 -0500

    Update aio.sh
    update

--- extras/aio.sh
+++ extras/aio.sh
@@ -1,14 +1,15 @@
-export vnd="18.0.0"
-export vrb="3.1.0"
-export vyr="1.19.1-1"
-nvm install v$vnd
-nvm use v$vnd
-nvm alias default v$vnd
+curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/nvm.sh
+VND="18.0.0"
+VRB="3.1.0"
+VYR="1.19.1-1"
+nvm install v$VND
+nvm use v$VND
+nvm alias default v$VND
 npm install -g npm@latest
-rvm install $vrb
-rvm use $vrb --default
-gem install rails --version $vrb
+rvm install $VRB
+rvm use $VRB --default
+gem install rails --version $VRB
 sudo gem install bundler
-sudo apt install -y yarn=$vyr
+sudo apt install -y yarn=$VYR
 sudo npm -g install yarn
 #sudo reboot
