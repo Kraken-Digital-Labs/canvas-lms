f599c68d4b87ab67b9f39b64054ad1a13632fd32
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-02 15:41:58 -0500

    Update hst.sh
    update

--- extras/hst.sh
+++ extras/hst.sh
@@ -1,6 +1,5 @@
 echo "Type your FQDN"
 read -p "Username: " fqdnx
-$FQDNX=$fqdnx
-export FQDN="$FQDNX"
+export FQDN="$fqdnx"
 hostnamectl set-hostname $fqdnx
 #hostnamectl set-hostname lms.paisdelconocimiento.org
