1c9ff3f0c6a72588117e867ae52f0bef69d9c15a
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-03 12:30:58 -0500

    Update clmsinit.sh
    update

--- extras/clmsinit.sh
+++ extras/clmsinit.sh
@@ -1,3 +1,6 @@
 sudo ln -s /var/canvas/script/canvas_init /etc/init.d/canvas_init
 sudo update-rc.d canvas_init defaults
 sudo /etc/init.d/canvas_init start
+cd /var/canvas/canvas-rce-api
+screen -S canvas-rce-api
+npm run start
