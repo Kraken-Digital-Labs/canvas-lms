76d9854631ebb86a80b8f7b6ff1eb9f48a45af19
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-03 12:46:30 -0500

    Update apch.sh
    update

--- extras/apch.sh
+++ extras/apch.sh
@@ -1,1 +1,9 @@
+
+sudo usermod -aG www-data $USER
+sudo unlink /etc/apache2/sites-enabled/000-default.conf
+sudo unlink /etc/apache2/sites-enabled/000-default-ssl.conf
+
+
+sudo a2enmod proxy_http
+sudo service apache2 restart
 sudo apachectl -M | sort
