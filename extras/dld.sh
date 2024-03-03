2a2b2d48a896933d9d6b0e7b527ac18ebde130f6
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-03 13:54:19 -0500

    Update dld.sh
    update

--- extras/dld.sh
+++ extras/dld.sh
@@ -6,5 +6,5 @@ sudo chmod -R u=rwx,go=rx
 cd /var/canvas/config/environments/
 wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/environments/production-local.rb
 cd /var/canvas/config/
-https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/yml.sh | bash
+curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/yml.sh | bash
 curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/aconf.sh | bash
