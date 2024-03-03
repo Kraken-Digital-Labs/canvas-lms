685329f0e72e7624cc8b38428bba3f6bd6661abe
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-03 00:45:06 -0500

    Create dld.sh
    update

---
+++ extras/dld.sh
@@ -0,0 +1,9 @@
+cd /var/canvas/config/environments/
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/environments/production-local.rb
+cd /var/canvas/config/
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/security.yml
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/redis.yml
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/outgoing_mail.yml
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/external_migration.yml
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/dynamic_settings.yml
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/domain.yml
