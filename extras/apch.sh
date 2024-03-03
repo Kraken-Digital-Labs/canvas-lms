081b638c592691dab396a855bb440a30522cba6f
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-03 12:53:57 -0500

    Update apch.sh
    update

--- extras/apch.sh
+++ extras/apch.sh
@@ -3,7 +3,9 @@ sudo apt-get install -y libapache2-mod-passenger
 sudo usermod -aG www-data $USER
 sudo unlink /etc/apache2/sites-enabled/000-default.conf
 sudo unlink /etc/apache2/sites-enabled/000-default-ssl.conf
-
+sudo a2ensite canvas.conf
+sudo a2ensite canvas-ssl.conf
+sudo service apache2 restart
 
 sudo a2enmod proxy_http
 sudo service apache2 restart
