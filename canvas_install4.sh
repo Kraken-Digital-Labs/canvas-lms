c81233e8cee1888c0d5da14c7d92bbae75f0bff7
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-03 14:19:29 -0500

    Update canvas_install4.sh
    update

--- canvas_install4.sh
+++ canvas_install4.sh
@@ -1,7 +1,5 @@
 bash
-#Este paso llena la base de datos de postgres
-curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/psql.sh | bash
-#Este paso llena la base de datos de postgres
+#Este paso configura la base de datos de postgres
 curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/psql.sh | bash
 #Este paso instala Canvas LMS
 curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/inscanvas.sh | bash
@@ -10,9 +8,9 @@ curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master
 #Este paso configura la instalación de Canvas RCE
 curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/rce.sh | bash
 #Este paso es la postconfiguración de Canvas RCE
-
+curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/eco.sh | bash
 #Este paso configura la configuración posterior de apache
-
+https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/apch.sh
 #bajar el ultimo script para terminar
 wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/canvas_install5.sh | bash
 sh canvas_install5.sh
