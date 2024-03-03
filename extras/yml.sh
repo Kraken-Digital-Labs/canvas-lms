8a691e95964f32a576580d30b8e5474624f74d57
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-03 13:46:53 -0500

    Create yml.sh
    update

---
+++ extras/yml.sh
@@ -0,0 +1,11 @@
+bash
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/database.yml
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/security.yml
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/redis.yml
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/outgoing_mail.yml
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/external_migration.yml
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/dynamic_settings.yml
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/domain.yml
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/cache_store.yml
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/canvas.conf
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/canvas-ssl.conf
