cbaf052bad55fec374fc950490c9c0cb5b7c0fa5
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-03 14:09:12 -0500

    Update xtr.sh
    update

--- extras/xtr.sh
+++ extras/xtr.sh
@@ -3,15 +3,6 @@ current_user=$(whoami)
 sudo chown -R "$current_user":"$current_user" .
 echo "host all all 0.0.0.0/0 md5" | sudo tee -a /etc/postgresql/16/main/pg_hba.conf
 sudo systemctl restart postgresql
-touch .env
-cat <<< "CANVAS_LMS_ADMIN_EMAIL=\"juan.villa@paisdelconocimiento.org\"
-CANVAS_LMS_ADMIN_PASSWORD=\"CambiarmeAlEntrar.*\"
-CANVAS_LMS_ACCOUNT_NAME=\"Juan Fernando Villa\"
-CANVAS_LMS_STATS_COLLECTION=\"opt_in\"" > .env
-ECOSYSTEM_SECRET=$(openssl rand -hex 32)
-ECOSYSTEM_KEY=$(openssl rand -hex 32)
-CIPHER_PASSWORD=$(openssl rand -hex 16)
-sed -i "s/^\(NODE_ENV=\).*/\1production/; s/^\(ECOSYSTEM_SECRET=\).*/\1$ECOSYSTEM_SECRET/; s/^\(ECOSYSTEM_KEY=\).*/\1$ECOSYSTEM_KEY/; s/^\(CIPHER_PASSWORD=\).*/\1$CIPHER_PASSWORD/" .env
 sudo find config/ -type f -exec chmod 400 {} +
 sudo chown $USER config/*.yml
 sudo chmod 400 config/*.yml
