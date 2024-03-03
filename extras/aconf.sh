fc2b9b05f2d4a16789a4f8148058a6af3e8a8da4
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-03 13:57:23 -0500

    Update aconf.sh
    update

--- extras/aconf.sh
+++ extras/aconf.sh
@@ -1,5 +1,8 @@
 bash
 cd /var/canvas/
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/canvas.conf
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/canvas-ssl.conf
 sudo mv canvas.conf /etc/apache2/sites-available/
 sudo mv canvas-ssl.conf /etc/apache2/sites-available/
 sudo mv passenger.conf /etc/apache2/mods-available/passenger.conf
+
