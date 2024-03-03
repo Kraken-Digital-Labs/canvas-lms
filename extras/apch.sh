20188fa5ae1c569436f534bf2d1cda8e69f167f4
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-03 13:37:56 -0500

    Update apch.sh
    update

--- extras/apch.sh
+++ extras/apch.sh
@@ -2,15 +2,11 @@ bash
 sudo apt-get install -y libapache2-mod-passenger
 sudo usermod -aG www-data $USER
 sudo apt update
-sudo apt install python3-certbot-apache
-sudo apt install certbot
+curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/ssl.sh | bash
 curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/cronjob.sh | bash
-sudo unlink /etc/apache2/sites-enabled/000-default.conf
-sudo unlink /etc/apache2/sites-enabled/000-default-ssl.conf
-sudo a2ensite canvas.conf
-sudo a2ensite canvas-ssl.conf
+curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/ulink.sh | bash
+curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/e2essslc.sh | bash
 sudo service apache2 restart
-
 sudo a2enmod proxy_http
 sudo service apache2 restart
 sudo apachectl -M | sort
