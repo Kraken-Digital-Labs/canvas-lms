d8e92f58c1379b7d53fd994e58de501193c57d2b
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-03 11:23:58 -0500

    Update aio.sh
    update

--- extras/aio.sh
+++ extras/aio.sh
@@ -10,6 +10,7 @@ nvm alias default v18.17.0
 npm install -g npm@latest
 rvm install 3.1.0
 rvm use 3.1.0 --default
+sudo apt-get install libyaml-dev
 gem install rails --version 3.1.0
 gem install bundler
 sudo apt install -y yarn=1.19.1-1
