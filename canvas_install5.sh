7bc660713c6e13248527657994b382d9829d3e88
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-03 12:17:48 -0500

    Update canvas_install5.sh
    update

--- canvas_install5.sh
+++ canvas_install5.sh
@@ -1,2 +1,3 @@
 bash
+curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/xtr.sh | bash
 curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/clmsinit.sh | bash
