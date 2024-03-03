ceccf5f16b039fe87b592f22dadbc0169155e267
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-03 11:02:04 -0500

    Update canvas_install4.sh

--- canvas_install4.sh
+++ canvas_install4.sh
@@ -1,1 +1,12 @@
-canvas_install4.sh
+#Este paso llena la base de datos de postgres
+https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/psql.sh
+#Este paso llena la base de datos de postgres
+#Este paso instala Canvas LMS
+
+#este paso llena la base de datos de canva
+https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/pdb.sh
+#Este paso configura la instalación de Canvas RCE
+
+#Este paso es la postconfiguración de Canvas RCE
+
+#Este paso configura la configuración posterior de apache
