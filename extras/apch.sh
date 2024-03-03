8312282aea82b25d74c2aed52f6f79df8460d167
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-03 13:42:57 -0500

    Update apch.sh
    update

--- extras/apch.sh
+++ extras/apch.sh
@@ -1,12 +1,12 @@
 bash
-sudo apt-get install -y libapache2-mod-passenger
+sudo service apache2 restart
 sudo usermod -aG www-data $USER
 sudo apt update
+curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/passgr.sh | bash
 curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/ssl.sh | bash
 curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/cronjob.sh | bash
 curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/ulink.sh | bash
 curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/e2essslc.sh | bash
-sudo service apache2 restart
 sudo a2enmod proxy_http
 sudo service apache2 restart
 sudo apachectl -M | sort
