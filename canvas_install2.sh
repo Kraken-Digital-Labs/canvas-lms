5c204149896729d8e84c58cd4c3230be854c69f3
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-03 00:04:32 -0500

    Update canvas_install2.sh
    update

--- canvas_install2.sh
+++ canvas_install2.sh
@@ -4,3 +4,7 @@ curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master
 curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/vrs.sh | bash
 curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/venvr.sh |bash
 curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/dxc.sh | bash
+
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/canvas_install3.sh
+echo "Reboot the system & then run canvas_install3.sh"
+sudo reboot
