32259bea06a6ba1dfd9045f52b03792914f3e9e9
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-03 13:52:37 -0500

    Create aconf.sh
    updates

---
+++ extras/aconf.sh
@@ -0,0 +1,5 @@
+bash
+cd /var/canvas/
+sudo mv canvas.conf /etc/apache2/sites-available/
+sudo mv canvas-ssl.conf /etc/apache2/sites-available/
+sudo mv passenger.conf /etc/apache2/mods-available/passenger.conf
