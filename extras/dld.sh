a6287dd73f7892a6d2e41540c365c4c096667c44
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-03 08:42:00 -0500

    Update dld.sh
    update

--- extras/dld.sh
+++ extras/dld.sh
@@ -1,6 +1,11 @@
+cd /var/canvas/
+current_user=$(whoami)
+sudo chown -R "$current_user":"$current_user" .
+sudo chmod -R u=rwx,go=rx
 cd /var/canvas/config/environments/
 wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/environments/production-local.rb
 cd /var/canvas/config/
+wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/database.yml
 wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/security.yml
 wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/redis.yml
 wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/outgoing_mail.yml
@@ -10,4 +15,5 @@ wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/con
 wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/config/cache_store.yml
 wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/canvas.conf
 wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/canvas-ssl.conf
-sudo mv canvas.conf /etc/var/
+sudo mv canvas.conf /etc/apache2/sites-available/
+sudo mv canvas-ssl.conf /etc/apache2/sites-available/
