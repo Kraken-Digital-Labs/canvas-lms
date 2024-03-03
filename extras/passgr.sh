895c7fcef537819ba054a8a074cbc3383da89edd
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-03 13:57:40 -0500

    Update passgr.sh
    update

--- extras/passgr.sh
+++ extras/passgr.sh
@@ -1,2 +1,5 @@
+sudo apt-get install -y libapache2-mod-passenger
+sudo a2enmod passenger
+sudo service apache2 restart
 sudo /usr/bin/passenger-config validate-install
 sudo /usr/sbin/passenger-memory-stats
