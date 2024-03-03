3aff80d1fd5acca4ebef1b6df2e8e018361859cc
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-03 08:51:05 -0500

    Update canvas_install2.sh
    update

--- canvas_install2.sh
+++ canvas_install2.sh
@@ -1,10 +1,12 @@
 bash
+#Este paso crea un archivo swap para aumentar el rendimiento de la maquina
 curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/swp.sh | bash
+#Este paso instala las aplicaciones que seran necesarias para ejecutar canvas lms
 curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/aio.sh | bash
+#Este paso verfica las aplicaciones que seran necesarias para ejecutar canvas lms
 curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/vrs.sh | bash
-curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/venvr.sh |bash
-curl -o- https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/extras/dxc.sh | bash
-
+#Este paso descarga el siguiente script necesario para descargar canvas y sus valores canvas lms
 wget https://raw.githubusercontent.com/Kraken-Digital-Labs/canvas-lms/master/canvas_install3.sh
 echo "Reboot the system & then run canvas_install3.sh"
+#reiniciamos para que se puedan tomar los cambios
 sudo reboot
