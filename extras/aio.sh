27c141f9ead8556029e2aad61368ba980b49ff58
Juan F Villa <158066926+juanfdovilla@users.noreply.github.com>
2024-03-03 19:42:41 -0500

    Update aio.sh
    update

--- extras/aio.sh
+++ extras/aio.sh
@@ -10,12 +10,12 @@ nvm alias default v18.17.0
 npm install -g npm@latest
 rvm install 3.1.0
 rvm use 3.1.0 --default
-sudo apt-get install libyaml-dev
 gem install rails --version 3.1.0
 gem install bundler
+sudo apt-get install libyaml-dev
 sudo apt install -y yarn=1.19.1-1
 npm -g install yarn
-sudo gem update strscan
-sudo gem uninstall stringio
-sudo gem install stringio -v 3.1.0
+gem update strscan
+gem uninstall stringio
+gem install stringio -v 3.1.0
 #sudo reboot
